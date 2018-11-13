ebc18553af0df241c3b584d3231a69178acdee23
David Santiago Iriarte Zamora <33665450+diriarte-condor@users.noreply.github.com>
2018-11-13 09:15:52 -0500

    Update setup_data_controller.rb

--- app/controllers/setup_data_controller.rb
+++ app/controllers/setup_data_controller.rb
@@ -1,5 +1,10 @@
 class SetupDataController < ApplicationController
-  def setup
+  
+   def setup
+    render json: generate_response, status: :ok
+  end
+  
+  def generate_response
     workplaces_json = File.read("#{Rails.root}/app/mocks/setupData.json")
     return workplaces_json
       end
