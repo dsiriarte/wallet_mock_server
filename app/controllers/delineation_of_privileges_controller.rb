0817cd57079bc40686f7b646be8a9ea0950b7086
Genesis Sanguino <gsanguino@condorlabs.io>
2019-03-19 11:06:50 -0500

    Delineation of privileges get added

--- app/controllers/delineation_of_privileges_controller.rb
+++ app/controllers/delineation_of_privileges_controller.rb
@@ -1,11 +1,12 @@
 class DelineationOfPrivilegesController < ApplicationController
-  
-    def setup
+    before_action :authenticate!, only: [:get_inbox_items]
+    
+    def get_privileges
      render json: generate_response, status: :ok
    end
    
    def generate_response
-     return File.read("#{Rails.root}/app/mocks/providerDelineationOfPrivileges.json")
+     return File.read("#{Rails.root}/app/mocks/provider/providerDelineationOfPrivileges.json")
        end
  end
  
